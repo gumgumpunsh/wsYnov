872fa08fcdb6951751fce141265f0a637df464cb
Nathan Piraux <61149320+nathomas34@users.noreply.github.com>
2024-03-29 12:47:47 +0100

    Update node_ws_dbinit_postgres.js

--- Src/node_ws_dbinit_postgres.js
+++ Src/node_ws_dbinit_postgres.js
@@ -2,7 +2,7 @@ require("node_ws_connect_postgres");
 
 con.connect(function(err) {
     if (err) throw err;
-    var sql = "CREATE TABLE ws_masks (id , address VARCHAR(255))";
+    var postgres = "CREATE TABLE ws_masks (id , address VARCHAR(255))";
     con.query(postgres, function (err, result) {
         if (err) throw err;
         console.log("Table créée !");
